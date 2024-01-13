9155158367cabdb6276a1dd354a26e3daa24018d
Лилия Байковская <liliabajkovskaa@gmail.com>
2024-01-13 15:39:50 +0200

    Add different types of variables

---
+++ HW_1.py
@@ -0,0 +1,47 @@
+""""Create variables john_salary and marta_salary of some type (floating point).
+Assign the created variables the values you want.
+Print the values of both variables to the console using the print method."""
+john_salary = 2000.0
+marta_salary = 3000.0
+print("John salary: ", john_salary)
+print("Marta salary: ", marta_salary)
+
+""""Create variables john_age and marta_age of integer type. 
+Assign the created variables the values you want. 
+Print the values of both variables to the console using the print method."""
+john_age = 30
+marta_age = 25
+print("John age: ", john_age)
+print("Marta age: ", marta_age)
+
+""""Create string type variables john_name and marta_name. 
+Assign the created variables the values you want.
+ Print the values of both variables to the console using the print method."""
+john_name = "John"
+marta_name = "Marta"
+print("John name: ", john_name)
+print("Marta name: ", marta_name)
+
+""""Create boolean variables john_gender and marta_gender.
+ Let john be false and Marta be true. 
+ Print the values of both variables to the console using the print method."""
+john_gender = False
+marta_gender = True
+print("john gender: ", john_gender)
+print("marta gender: ", marta_gender)
+
+""""Create variables john_friends and marta_friends. 
+Assign lists to variables. Each list must contain the names of friends. 
+John has his list of friends and Martha has hers. 
+Friends (friend's name) can be the usual strings "James", "Peter", etc."""
+john_friends = ["Nick", "Vova", "Igor"]
+marta_friends = ["Mia", "Zuza", "Oliwia", "Sofi"]
+print("John friends : ", john_friends)
+print("Marta friends : ", marta_friends)
+
+""""Create a list with people's names, some names should be repeated in it.
+ Turn a list of duplicate names into a list of unique names using sets."""
+people_names_list = ["Sem", "Vova", "Sem", "Zoja", "Vova"]
+print("people names list : ", people_names_list)
+unique_names_list = set(people_names_list)
+print("unique names list: ", unique_names_list)
