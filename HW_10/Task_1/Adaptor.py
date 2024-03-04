ca02574ba960b09fd8dbfa1c62a22da80ed071ca
Лилия Байковская <liliabajkovskaa@gmail.com>
2024-03-04 17:10:38 +0200

    Proxy, adapter

--- HW_10/Task_1/Adaptor.py
+++ HW_10/Task_1/Adaptor.py
@@ -4,12 +4,12 @@ class TxtAdaptor:
         with open(file_path) as f:
             lines = f.readlines()
 
-            header = [k.strip() for k in lines[0].split(',')]
-            rows = lines[1:]
-            for row in rows:
-                row = row.strip().split(',')
-                for i in range(len(header)):
-                    print(f'<{header[i]}>{row[i]}</{header[i]}>')
+        header = [k.strip() for k in lines[0].split(',')]
+        rows = lines[1:]
+        for row in rows:
+            row = row.strip().split(',')
+            for i in range(len(header)):
+                print(f'<{header[i]}>{row[i]}</{header[i]}>')
 
 
 TxtAdaptor.csv_to_xml('tst_file.txt')
