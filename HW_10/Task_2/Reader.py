5249e3643f28f39a8d28b8c0bcdc975cd1e3c7d6
Лилия Байковская <liliabajkovskaa@gmail.com>
2024-03-04 17:06:54 +0200

    Proxy, adapter

--- HW_10/Task_2/Reader.py
+++ HW_10/Task_2/Reader.py
@@ -2,10 +2,7 @@ class Reader:
     def __init__(self, file_path):
         self.__file_path = file_path
         self.data = None
-        self.file_read = False
 
     def read_file(self):
-        if not self.file_read:
-            with open(self.__file_path, 'r') as f:
-                self.data = f.read()
-            self.file_read = True
+        with open(self.__file_path, 'r') as f:
+            self.data = f.read()
